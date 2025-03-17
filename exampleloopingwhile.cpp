adbc055b1cb4b9a2cbd0d55746dc20eab88f6504
mansoor <t.alyamani95@gmail.com>
2025-03-17 19:56:48 +0700

    jhhjhj

---
+++ exampleloopingwhile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int counter;
+    int i;
+    int perulangankwhile = 0;
+    int perulanganDo = 0;
+
+   
+
+
+
+}
